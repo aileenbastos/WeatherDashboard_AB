6cb0b65aa5d07319e52e53f2d463c6cfba5c181b
davidsdottir <aileen.bastos@gmail.com>
2021-09-05 15:08:53 -0400

    working code!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,175 @@
-// Element values
-const citySearchInput = document.querySelector('#city-search');
-const searchButtonEl = document.querySelector('.search-btn');
+var searchCityEl = document.getElementById("searchCity");
+var cityHeadingEl = document.querySelector(".cityNameDate");
+//api key from profile
+var apiKey = "&appid=b1753ec59a219420447810a8ba1a0092";
+// Declares a 'list' variable that holds the parsed searched cities retrieved from 'localStorage'
+// If there is nothing in 'localStorage', sets the 'list' to an empty array
+var searchedCities = JSON.parse(localStorage.getItem('searchedCities')) || [];
 
-const currentWeatherEl = document.querySelector('.current-weather-container');
-const cityDay = document.querySelector('.city-name-date');
-const buttonsContainerEl = document.querySelector('.new-buttons');
+//display searched city list
+function displaySearchedCities() {
+    for(i = 0; i < searchedCities.length; i++) {
+        //for each searched city create list item
+        var searchedCityListEl = document.createElement("li");
+        searchedCityListEl.classList = "searchedCity";
+        var searchedCityBtn = document.createElement("button");
+        searchedCityBtn.classList = "btn cityBtn";
+        searchedCityBtn.textContent = searchedCities[i];
+        searchedCityListEl.appendChild(searchedCityBtn);
+        $(".searchedCitiesList").append(searchedCityListEl);
+    }
+}
+function getCity(city) {
+    //show city and date in currentWeather div
+    var date = moment().format("MM/DD/YYYY");
+    city = searchCityEl.value.trim() || city;
+    cityHeadingEl.textContent = city + " (" + date + ")";
 
-const forecastContainerEl = document.querySelector('.five-day-container');
-const weatherCardsContainerEl = document.querySelector('.forecast-container');
+    //save city to local storage
+    searchedCities.push(city);
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+    //show cities in list
 
-// Add weather info through p elements
-let temp = document.createElement('p');
-let wind = document.createElement('p');
-let humidity = document.createElement('p');
-let uvi = document.createElement('p');
-let description = document.createElement('p');
 
-// Current date
-let currentDate = moment().format('M/DD/YYYY');
+    //format weather api url to get long/lat
+    var weatherApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + apiKey;
 
-// Global local storage value -- if nothing stored, make an empty array
-let storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
-
-// API key for OpenWeather fetches
-const API_KEY = '1fd353df9a65fba742aece36f28e320a';
-
-// * DISPLAY PERSISTING CITY BUTTONS
-function displayButtons() {
-
-   // Check and get local storage array before saving
-   storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
-
-   // Traverse through stored cities backwards (most recently searched shows first)
-   for (i = storedCities.length - 1; i >= 0; i--) {
-      // Create a new button element
-      var cityButton = document.createElement('button');
-      // Set classes, id, and text
-      cityButton.classList = 'btn city-btn';
-      cityButton.textContent = storedCities[i];
-      // Append the button to the container
-      buttonsContainerEl.append(cityButton);
-   };
-};
-
-// * CREATE A PERSISTENT CITY BUTTON FROM NEW SEARCH
-function createButton(city) {
-   // Create a new button 
-   var newCityButton = document.createElement('button');
-   newCityButton.classList = 'btn city-btn';
-   // Label it with its city
-   newCityButton.textContent = city;
-   // Append the button to its container
-   buttonsContainerEl.append(newCityButton);
-
-   // Pass the city to the fetch function
-   fetchCoordinates(city);
-   // Clear search input on page
-   citySearchInput.value = '';
+    //make a request to the url 
+    fetch(weatherApiUrl).then(function(response) {
+       //if no 404 error/ response.ok = successful, if not alert if there is a 404 error
+       if(response.ok) {
+        response.json().then(function(data) {
+            getCurrentWeather(data);
+        });
+    } else {
+        alert("Please enter a valid city.");
+    }
+})
+    .catch(function(error) {
+        //catch 
+        alert("Unable to connect to One Call")
+    })
 }
 
-// * DISPLAY SEARCHED CITY NAME AND SAVE TO LOCAL STORAGE
-function getCityInfo(event) {
-   event.preventDefault();
+function getCurrentWeather(city) {
+    //clear input text box
+    searchCityEl.value = "";
+    //get latitude and longitude for city to pass into fetch request
+    var latitude = city[0].lat;
+    var longitude = city[0].lon;
 
-   // Clear current city title and forecast
-   cityDay.textContent = '';
-   forecastContainerEl.innerText = '';
-   weatherCardsContainerEl.innerText = '';
-   // Get the input value submitted
-   var city = citySearchInput.value.trim();
-   // Check for valid input 
-   if (!city) {
-      alert(`You didn't enter anything. Give it another go.`);
-      return;
-   }
+    var apiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + apiKey; 
 
-   // Check and get local storage array before saving
-   storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
-   // Save city to local storage
-   storedCities.push(city);
-   storedCities = localStorage.setItem('storedCities', JSON.stringify(storedCities));
+    fetch(apiUrl).then(function(response) {
+        //if no error 
+        if(response.ok) {
+            response.json().then(function(data) {
+                //get main weather icon
+                var weatherIconInfo = data.current.weather[0].icon;
+                var weatherIconEl = document.createElement("img");
+                weatherIconEl.src = "https://openweathermap.org/img/wn/" + weatherIconInfo + ".png";
+                cityHeadingEl.appendChild(weatherIconEl);
 
-   // Create a button for the city to persist on the page
-   createButton(city);
-};
+                //get info for current weather div list 
+                var currentTemp = data.current.temp;
+                var currentWindSpeed = data.current.wind_speed;
+                var currentHumidity = data.current.humidity;
+                var currentUvIndex = data.current.uvi;
+                //for uv element to create color coding of value
+                var currentUvEl = document.createElement("span");
+                currentUvEl.textContent = currentUvIndex;
+                if(currentUvIndex <= 2) {
+                    currentUvEl.classList = "favorable";
+                } else if(currentUvIndex > 2 && currentUvIndex < 6) {
+                    currentUvEl.classList = "moderate";
+                } else if (currentUvIndex >= 6) {
+                    currentUvEl.classList = "severe";
+                }
+                //show it on the current weather div
+                $(".temp").text("Temp: " + currentTemp + "\u00B0 F"); 
+                $(".wind").text("Wind: " + currentWindSpeed + " MPH");
+                $(".humidity").text("Humidity: " + currentHumidity + " %");
+                $(".uvIndex").text("UV Index: ").append(currentUvEl);
 
-// ! Get info from local storage and display persistent buttons
-displayButtons();
+                fiveDayForecast(latitude, longitude);
+                addSearchedCity();
+                
+            })
+        } 
+    })
+    .catch(function(error) {
+        //catch 
+        alert("Unable to connect to One Call")
+    });
+}
+//five day forecast
+function fiveDayForecast(latitude, longitude){
+    //clear five day forecast div to just show the heading prior to creating new elements for 5 day forecast
+    $(".forecast").html("<h3 class='forecastHeading'>5-Day Forecast:</h3>");
+    var forecastApiUrl ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current,minutely,hourly,alerts" + "&units=imperial" + apiKey; 
+    //fetch 
+    fetch(forecastApiUrl).then(function(response) {
+        //if no error 
+        if(response.ok) {
+            response.json().then(function(data) {
+                var dailyArray = data.daily;
 
-// ! Get city info on search button click
-searchButtonEl.addEventListener('click', getCityInfo);
+                for(i = 1; i < 6; i++) {
+                    //create div to hold each day info
+                    var forecastDay = document.createElement("div");
+                    forecastDay.classList = "forecastDay col";
+                    $(".forecast").append(forecastDay);
+                    //show date
+                    var unixDate = data.daily[i].dt;
+                    var dateToShow = moment.unix(unixDate).format("MM/DD/YYYY");
+                    var dayDateEl = document.createElement("h4");
+                    dayDateEl.classList = "dailyForecastDate";
+                    dayDateEl.textContent = dateToShow;
+                    forecastDay.appendChild(dayDateEl);
 
-// ! Search city already stored on existing city button
-$(document).on('click', '.city-btn', function (event) {
-   // Clear current city title and forecast
-   cityDay.textContent = '';
-   forecastContainerEl.innerText = '';
-   weatherCardsContainerEl.innerText = '';
-   // Display the date and the city and fetch the coordinates
-   var storedCity = event.target.textContent;
-   cityDay.textContent = `${storedCity} (${currentDate})`;
-   fetchCoordinates(storedCity);
-});
+                    //show weather icon for  each day
+                    var dailyIcon = data.daily[i].weather[0].icon;
+                    var dailyIconEl = document.createElement("img");
+                    dailyIconEl.src = "https://openweathermap.org/img/wn/" + dailyIcon + ".png";
+                    forecastDay.appendChild(dailyIconEl);
+
+                    //show daily temp for the day
+                    var dailyTemp = document.createElement("p");
+                    dailyTemp.textContent = "Temp: " + data.daily[i].temp.day + "\u00B0 F";
+                    forecastDay.appendChild(dailyTemp);
+
+
+                    //show daily wind speed for the day
+                    var dailyWind = document.createElement("p");
+                    dailyWind.textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+                    forecastDay.appendChild(dailyWind);
+                    
+                    // show daily humidity
+                    var dailyHumidity = document.createElement("p");
+                    dailyHumidity.textContent = "Humidity: " + data.daily[i].humidity + "%";
+                    forecastDay.appendChild(dailyHumidity);
+                }
+            })
+        }
+    })
+}
+
+function addSearchedCity() {
+       //add city to searched city list with button 
+       var lastSearchedCityList = document.createElement("li")
+       lastSearchedCityList.classList = "searchedCity";
+       var lastSearchedCityBtn = document.createElement("button");
+       lastSearchedCityBtn.classList = "btn cityBtn";
+       lastSearchedCityBtn.textContent = searchedCities[searchedCities.length - 1];
+       lastSearchedCityList.appendChild(lastSearchedCityBtn);
+       $(".searchedCitiesList").append(lastSearchedCityList);
+}
+
+//when click button to search, functions begin
+$(".searchBtn").on("click", getCity);
+// add city button functionality
+$(document).on("click", ".cityBtn", function() {
+    var btnCity = event.target.textContent
+    getCity(btnCity);
+})
+displaySearchedCities();
